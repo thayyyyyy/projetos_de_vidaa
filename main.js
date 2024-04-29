6f96eaf543cff7a805b9b1a8758b383700443b62
Thayná Ribeiro <108530408+thayyyyyy@users.noreply.github.com>
2024-04-29 11:09:22 -0300

    s

--- main.js
+++ main.js
@@ -185,11 +185,11 @@ for (let i = 0; i < contadores.length; i++) {
 
 // definição do tempo dos objetivos
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-30T00:00:00");
+const tempoObjetivo1 = new Date("2026-04-30T00:00:00");
 const tempoObjetivo2 = new Date("2024-10-15T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-06T00:00:00");
-const tempoObjetivo4 = new Date("2025-12-30T00:00:00");
-const tempoObjetivo5 = new Date("2025-11-27T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-15T00:00:00");
+const tempoObjetivo5 = new Date("2025-06-27T00:00:00");
 
 //automatizar o código, criar uma variável chamada tempos, que receberá uma lista.
 const tempos =
